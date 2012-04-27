caaea1fda2b416a3455bbbbb42ab6f55559bc38b
James Cleveland <jamescleveland@gmail.com>
2012-04-27 19:07:47 +0100

    arm cache

--- suave/utils.py
+++ suave/utils.py
@@ -4,13 +4,12 @@ from .models import Page
 
 def get_page_from_url(url):
     key = 'suave:page_at_url:%s' % url
-    #page = cache.get(key)
-    page = None
+    page = cache.get(key)
     if page == None:
         page = False
         for attempt in Page.objects.live().all():
             if attempt.url.strip('/') == url.strip('/'):
                 page = attempt
                 break
-    cache.set(key, page, 20)
+    cache.set(key, page, 60)
     return page
