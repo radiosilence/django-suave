57447f4ca52be8615bede3910f740d013704e3ff
James Cleveland <jamescleveland@gmail.com>
2012-05-22 19:47:05 +0100

    sql optimisation

--- suave/utils.py
+++ suave/utils.py
@@ -6,10 +6,16 @@ def get_page_from_url(url):
     key = 'suave:page_at_url:%s' % url
     page = cache.get(key)
     if page == None:
-        page = False
-        for attempt in Page.objects.live().all():
-            if attempt.url.strip('/') == url.strip('/'):
-                page = attempt
-                break
+        if url[-1] != '/':
+            url = url + '/'
+        if url == '/':
+            page = Page.objects.all()[0].get_root()
+        else:
+            page = False
+            slug = url.split('/')[-2]
+            for attempt in Page.objects.filter(slug=slug).live().all():
+                if attempt.url.strip('/') == url.strip('/'):
+                    page = attempt
+                    break
     cache.set(key, page, 60)
     return page
