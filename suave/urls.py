72dd8d253413deec3490924ba6ad6e01f2b7cec5
James Cleveland <jamescleveland@gmail.com>
2012-06-18 15:21:35 +0100

    fixed bug that disallowed numbers in slugs

--- suave/urls.py
+++ suave/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('suave.views',
-    url(r'^(?P<url>[a-z\-\/]*)/$', 'page', name='page'),
+    url(r'^(?P<url>[a-z0-9\-\/]+)/$', 'page', name='page'),
     url(r'^$', 'page', name='page'),
 )
