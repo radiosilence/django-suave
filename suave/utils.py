7c9e1b5b6f3bb375a30516bb3a40c6b4b6898bf6
James Cleveland <jamescleveland@gmail.com>
2012-08-29 21:07:10 +0100

    add link field

--- suave/utils.py
+++ suave/utils.py
@@ -4,7 +4,9 @@ from functools import wraps
 from django.http import Http404
 from django.conf import settings
 from django.utils.importlib import import_module
+from django.utils.safestring import mark_safe
 from django.db.models.signals import pre_save, post_save
+from django.core.urlresolvers import reverse_lazy, reverse
 from django.dispatch import receiver
 
 from .models import Page, pre_route, post_route, Image
@@ -40,3 +42,20 @@ def get_default_image():
         return Image(image=settings.DEFAULT_IMAGE)
     except AttributeError:
         return None
+
+
+def add_link_field(target_model=None, field='', link_text=unicode):
+    def add_link(cls):
+        reverse_name = target_model or cls.model.__name__.lower()
+        def link(self, instance):
+            link_obj = getattr(instance, field, None) or instance
+            app_name = link_obj._meta.app_label
+            reverse_path = "admin:{}_{}_change".format(app_name, reverse_name)
+            url = reverse(reverse_path, args=(link_obj.id,))
+            return mark_safe("<a href='%s'>%s</a>" % (url, link_text(link_obj)))
+        link.allow_tags = True
+        link.short_description = reverse_name + ' link'
+        cls.link = link
+        cls.readonly_fields = list(getattr(cls, 'readonly_fields', [])) + ['link']
+        return cls
+    return add_link
