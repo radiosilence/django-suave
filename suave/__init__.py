b5498842108b42f14802d62d42f5473ba0fac856
James Cleveland <jamescleveland@gmail.com>
2012-09-14 12:26:51 +0100

    actual snippet code

--- suave/__init__.py
+++ suave/__init__.py
@@ -1,13 +1,16 @@
-from django.template import Context, loader
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-template_cache = {}
-def render_cached(template_name, dictionary=None):
+from django.template import loader
+from django.conf import settings
+
+
+template_cache = {} 
+original_get_template = loader.get_template
+def cached_get_template(template_name):
     global template_cache
-        
-    t = template_cache.get(template_name, None)
+    t = template_cache.get(template_name,None)
     if not t or settings.DEBUG:
-        template_cache[template_name] = t = loader.get_template(template_name)
-
-    dictionary = dictionary or {}    
-    context_instance = Context(dictionary)
-    return t.render(context_instance)
+        template_cache[template_name] = t = original_get_template(template_name)
+    return t
+loader.get_template = cached_get_template
