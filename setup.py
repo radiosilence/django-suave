5435afc79d07c8c508390aae7b9e0b36e745757a
James Cleveland <jamescleveland@gmail.com>
2012-12-02 14:02:35 +0000

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-suave',
-    version="0.4.8",
+    version="0.5.0",
     description='Rather nice pages.',
     long_description=open('README.rst').read(),
     url='https://github.com/radiosilence/django-suave',
