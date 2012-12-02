d327d2373eabeb9252b8ac9dc007bd2d9456c99b
James Cleveland <jamescleveland@gmail.com>
2012-12-02 14:08:22 +0000

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
+requires = []
+dep_links = []
+
+for dep in open('requirements.txt').read().split("\n"):
+    if dep.startswith('git+'):
+        dep_links.append(dep)
+    else:
+        requires.append(dep)
+
 setup(
     name='django-suave',
     version="0.5.0",
@@ -11,5 +20,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     license="LICENSE.txt",
-    install_requires=open('requirements.txt').read().split("\n")
+    install_requires=requires,
+    dependency_links=dep_links,
 )
