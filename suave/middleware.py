daaee4678f71584c6cbe94f381fef5dd9d6bab6d
James Cleveland <jamescleveland@gmail.com>
2012-12-10 15:58:24 +0000

    some tweaks to view processing eg. not stripping path, and making middleware process_request

--- suave/middleware.py
+++ suave/middleware.py
@@ -2,11 +2,8 @@ from .models import pre_route
 
 
 class PreRouteMiddleWare(object):
-    def process_view(self, request, *args, **kwargs):
-        url = request.path.strip('/')
-        if url == '':
-            url = '/'
-        pre_routes = pre_route.send(sender=request, url=url)
+    def process_request(self, request, *args, **kwargs):
+        pre_routes = pre_route.send(sender=request, url=request.path)
         for reciever, response in pre_routes:
             if response:
                 return response
