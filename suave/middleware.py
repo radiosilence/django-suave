ec81e6d18edc256b466caaf895f4bba4b8ab3b2b
James Cleveland <jamescleveland@gmail.com>
2012-12-17 17:21:59 +0000

    it was being funky on process-request

--- suave/middleware.py
+++ suave/middleware.py
@@ -2,7 +2,7 @@ from .models import pre_route
 
 
 class PreRouteMiddleWare(object):
-    def process_request(self, request, *args, **kwargs):
+    def process_view(self, request, *args, **kwargs):
         pre_routes = pre_route.send(sender=request, url=request.path)
         for reciever, response in pre_routes:
             if response:
